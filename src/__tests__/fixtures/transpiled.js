bf32e96524b0fd55ee22c25f6d0b0bb0cb75fdfb
Kent C. Dodds <kent@doddsfamily.us>
2017-07-05 00:23:58 -0600

    test: fix broken test

--- src/__tests__/fixtures/transpiled.js
+++ src/__tests__/fixtures/transpiled.js
@@ -1,4 +1,4 @@
 export default preval`
   import path from 'path'
-  export default path.join(__dirname, 'something/whatever.js')
+  export default path.join(__dirname, 'something/whatever.js').replace(__dirname, '')
 `
