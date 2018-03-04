4a362db1fd00dec8b1b4ee9715494558181bd4c3
marzelin <marzelin@gmail.com>
2018-03-04 20:16:46 +0100

    (fix) perform transpilation for modules required in preval's template tags

--- src/get-replacement.js
+++ src/get-replacement.js
@@ -4,9 +4,10 @@ const objectToAST = require('./object-to-ast')
 module.exports = getReplacement
 
 function getReplacement({string: stringToPreval, filename, babel}) {
-  const {code: transpiled} = babel.transform(stringToPreval, {
+  const {code} = babel.transform(stringToPreval, {
     filename,
   })
+  const transpiled = `require('babel-register');\n${code}`
   const val = requireFromString(transpiled, filename)
   return objectToAST(val)
 }
